a1302fad4d112a2b249372b72de29cc4115d1e96
Julien Bordellier <julienbordellier@gmail.com>
2014-05-25 12:56:44 +0800

    Homework2 - PeerAssignment / Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Set of two functions to cache the matrix inversion operation.
+## The methods made available through the wrapped matrix are:
+## get() => return the original matrix data
+## set(matrix) => set a new matrix data in the wrapped matrix
+## getInverse() => return the last set inverse matrix
+## setInverse(inverse_matrix) => set a new inverse matrix according to the matrix data
 
-## Write a short comment describing this function
-
+## Function creating the CacheMatrix wrapper by returning a list of functions as described above.
 makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
 
+  get <- function() x
+  set <- function(new_x) {
+    x <<- new_x
+    inverse <<- NULL
+  }
+  
+  getInverse <- function() inverse
+  setInverse <- function(new_inverse) inverse <<- new_inverse
+  
+  list(
+    get = get,
+    set = set,
+    getInverse = getInverse,
+    setInverse = setInverse
+  )
 }
 
-
-## Write a short comment describing this function
-
+## Function returning the inverse matrix using the cached value if already calculated.
+## This is a memoized version of the solve function applied to the CacheMatrix object type.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if (is.null(inverse)) {
+    matrix <- x$get()
+    inverse <- solve(matrix)
+    x$setInverse(inverse)
+  }
+  inverse
 }
